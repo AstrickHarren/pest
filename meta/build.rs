bc7a20a45eb27ec5c18dff9c684337a0f057284d
Astrick <33888523+AstrickHarren@users.noreply.github.com>
2023-06-04 17:29:26 -0400

    hack(meta): stop dynmaic bootstrap

--- meta/build.rs
+++ meta/build.rs
@@ -21,48 +21,9 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let hash_path = out_dir.join("pest_hash.sha1");
 
-    // If `grammar.pest` exists (we're building from git sources)
-    if grammar_pest_path.exists() {
-        let mut sha = Sha256::default();
-
-        let old_hash = File::open(&hash_path).ok().map(|mut file| {
-            let mut s = String::new();
-            file.read_to_string(&mut s).unwrap();
-            s
-        });
-        let current_grammar = fs::read_to_string(grammar_pest_path).unwrap();
-        sha.update(current_grammar.as_bytes());
-        let current_hash = display_digest(&sha.finalize());
-
-        // If `grammar.pest` has changed
-        if !grammar_rs_path.exists()
-            || old_hash.as_ref().map(|it| it.trim()) != Some(current_hash.trim())
-        {
-            println!("Bootstrapping `meta/src/grammar.rs`");
-
-            let mut hash_file = File::create(hash_path).unwrap();
-            writeln!(hash_file, "{}", current_hash).unwrap();
-
-            // This "dynamic linking" is probably so fragile I don't even want to hear it
-            let status = Command::new(manifest_dir.join("../target/debug/pest_bootstrap"))
-                .spawn()
-                .unwrap_or_else(|_| {
-                    panic!(
-                        "Bootstrap failed because no bootstrap executable was found. \
-                         Please run `cargo build --package pest_bootstrap` or `cargo bootstrap` \
-                         and then try again.",
-                    )
-                })
-                .wait()
-                .unwrap();
-            assert!(status.success(), "Bootstrap failed");
-        } else {
-            println!("       Fresh `meta/src/grammar.rs`");
-        }
-    } else {
-        assert!(
-            grammar_rs_path.exists(),
-            "package is broken; does not contain grammar.rs"
-        );
-    }
+    // HACK: don't try to build pest grammar from pest file
+    assert!(
+        grammar_rs_path.exists(),
+        "package is broken; does not contain grammar.rs"
+    );
 }
